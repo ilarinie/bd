82150afabe02b28bd3f5caa790eff9e3e7ff2a2f
Ilari Nieminen <ilari.nieminen@barona.fi>
2024-02-27 15:27:28 +0200

    Fix some tests

--- src/services/staticExpenseService/__test__/createStaticExpense.test.ts
+++ src/services/staticExpenseService/__test__/createStaticExpense.test.ts
@@ -6,6 +6,7 @@ import { createStaticExpense } from '../createStaticExpense'
 import { Category } from '../../../utils/selectableCategories'
 import { prismaClient } from '../../../db'
 import { createUserIncome } from '../../userIncomeService'
+import { getOrCreateBudget } from '../../budgetService'
 
 describe('Create static expense', () => {
   let user: User | undefined
@@ -14,7 +15,7 @@ describe('Create static expense', () => {
     await createUserIncome(user!.id, { year: 2024, month: 0 }, 1000)
   })
 
-  it.only('should create a static expense', async () => {
+  it('should create a static expense', async () => {
     const staticExpense = await createStaticExpense(user!.id, 'Test static expense', 100, Category.FOOD)
     expect(staticExpense.amount).toBe(100)
     expect(staticExpense.description).toBe('Test static expense')
@@ -29,15 +30,15 @@ describe('Create static expense', () => {
     expect(budget).toBeNull()
   })
 
-  it.only('should create a static expense and add it to budget', async () => {
+  it('should create a static expense and add it to budget', async () => {
     const staticExpense = await createStaticExpense(user!.id, 'Test static expense', 100, Category.FOOD, { year: 2024, month: 0 })
     expect(staticExpense.amount).toBe(100)
     expect(staticExpense.description).toBe('Test static expense')
     expect(staticExpense.category).toBe(Category.FOOD)
 
-    // const budget = await getOrCreateBudget(user!.id)
+    const budget = await getOrCreateBudget(user!.id, { year: 2024, month: 0 })
 
-    // expect(budget).toBeDefined()
-    // expect(budget.purchases.length).toBe(1)
+    expect(budget).toBeDefined()
+    expect(budget.purchases.length).toBe(1)
   })
 })
